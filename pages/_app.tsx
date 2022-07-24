be9a80ba350bb350b196216a33733c9c8cea6660
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2022-07-24 13:35:28 -0300

    create favorites context. create load more function

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { FavoritesProvider } from "contexts/FavoritesContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <FavoritesProvider>
+      <Component {...pageProps} />
+    </FavoritesProvider>
+  );
 }
 
 export default MyApp;
